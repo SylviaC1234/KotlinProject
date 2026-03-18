7395dcba82fe911cf1fa5e58376da145053f244b
SylviaC1234 <SylviaC432@gmail.com>
2026-03-18 12:44:08 +0300

    new commit

---
+++ src/main/kotlin/ControlStatements.kt
@@ -0,0 +1,49 @@
+fun main() {
+    //Program to determine room condition
+    var temperature = 67
+
+    if (temperature > 25) {
+        println("It is too hot")
+          }
+
+    else if (temperature < 25) {
+        println("It is too cold")
+    }
+
+    else {
+        println("Normal temperature")
+    }
+
+
+    //Program to check for even or odd number
+
+    var number = 45
+
+    if (number%2==0){
+        println("It is even")
+    }
+
+    else {
+        println("It is odd")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/kotlin/loops.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //while loop
+    var number = 200
+
+    while (number <= 205) {
+        print("Number : $number " +"")
+        number ++
+    }
+
+
+}
